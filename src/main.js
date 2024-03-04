09e235e309a8d719a9ff8a39d10a4d37ca62e538
MSI\nonwhen07 <nonwhen07@gmail.com>
2024-03-04 14:06:55 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// Vue外部
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// Vue內部
+import App from './App.vue'
+import router from './router'
+
+// codeing
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios)
+app.component('LoadingOverlay',Loading)
+app.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <LoadingOverlay :active="isLoading"/>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+
+<script>
+
+export default {
+  data(){
+    return {
+      isLoading: true,
+    }
+  },
+  mounted(){
+    setTimeout(() => {
+      this.isLoading = false
+    }, 1000)
+  }
+}
+</script>
